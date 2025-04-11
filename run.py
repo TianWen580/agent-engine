063938fe8229f2995fa4ec980a1d3b2cd041c53b
林天文 <tianwen580@hotmail.com>
2025-04-11 11:30:55 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from agent_engine.utils import create_workflow
 
-workflow = create_workflow(config="my_configs/for_computer_vision/coco_cls_checker.yaml")
+workflow = create_workflow(
+    config="configs/for_computer_vision/coco_cls_checker.yaml")
 workflow.execute()
